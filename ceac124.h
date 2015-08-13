2ff818f1024c4eaf0b90e6239d8e6b1e88854f8c
Alexey Gerasev <alexey.gerasev@gmail.com>
2015-08-13 12:44:35 +0600

    architecture example

--- ceac124.h
+++ ceac124.h
@@ -1,10 +1,39 @@
 #pragma once
 
+#include "util.h"
 #include "device.h"
+
+typedef struct
+{
+	u8 dev_mode;
+	u8 label;
+	u16 padc;
+	u8 file_ident;
+	u16 pdac;
+}
+CEAC124_DeviceStatus;
+
+typedef struct
+{
+	u8 device_code;
+	u8 hw_version;
+	u8 sw_version;
+	u8 reason;
+}
+CEAC124_DeviceAttrib;
 
 typedef
 struct CEAC124
 {
-	CAN_Device device;
+	// device instance
+	CAN_Device can_device;
+	
+	// callbacks
+	void *cb_cookie; // callback user data
+	void (*cb_device_status)(void *, CEAC124_DeviceStatus);
+	void (*cb_device_attrib)(void *, CEAC124_DeviceAttrib);
 }
 CEAC124;
+
+int CEAC124_requestDeviceStatus(const CEAC124 *device);
+int CEAC124_requestDeviceAttrib(const CEAC124 *device);
